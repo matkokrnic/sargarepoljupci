283292a4934eb2ef3c1d39a0e96665086dd70b37
piilip <fm54235@fer.hr>
2023-11-11 23:27:09 +0100

    Vrlo primitivna verzija autentifikacije korisnika za login.

--- IzvorniKod/backend/src/main/java/com/progi/sargarepoljupci/Controllers/LoginController.java
+++ IzvorniKod/backend/src/main/java/com/progi/sargarepoljupci/Controllers/LoginController.java
@@ -1,13 +1,48 @@
 package com.progi.sargarepoljupci.Controllers;
 
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.progi.sargarepoljupci.Models.Korisnik;
+import com.progi.sargarepoljupci.Repository.KorisnikRepository;
+import com.progi.sargarepoljupci.Services.KorisnikService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/login")
 public class LoginController {
+
+    private final KorisnikService korisnikService;
+
+    private final KorisnikRepository korisnikRepository;
+    private final BCryptPasswordEncoder encoder;
+    @Autowired
+    public LoginController(KorisnikService userService, KorisnikRepository korisnikRepository, BCryptPasswordEncoder encoder) {
+        this.korisnikService = userService;
+        this.korisnikRepository = korisnikRepository;
+
+        this.encoder = encoder;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> loginKorisnik(@RequestBody Korisnik korisnik){
+        Optional<Korisnik> existingUser = korisnikRepository.findByEmail(korisnik.getEmail());
+        if(existingUser.isPresent()){
+            if(encoder.matches(korisnik.getEmail(), existingUser.get().getEmail()) && korisnik.getKorisnickoIme().equals(existingUser.get().getKorisnickoIme())){
+               return ResponseEntity.status(HttpStatus.ACCEPTED).body("Ulogiran");
+            }else{
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Uneseni podatci su neispravni");
+            }
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Korisnik ne postoji");
+        }
+
+    }
 
 
 
