42434c9c9e7617fa9b4ac5e77f0dd6251c7befa0
piilip <fm54235@fer.hr>
2023-12-11 21:11:18 +0100

    Autentifikacija sa JWT tokenom i zaštićeni su ADMIN endpointi

---
+++ IzvorniKod/backend/src/main/java/com/progi/sargarepoljupci/Security/WebSecurityConfig.java
@@ -0,0 +1,74 @@
+package com.progi.sargarepoljupci.Security;
+
+import com.progi.sargarepoljupci.Security.JWT2.JWTAuthenticationFilter;
+import com.progi.sargarepoljupci.Services.korisnikDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private final JWTAuthenticationFilter authenticationFilter;
+
+    private final korisnikDetailsService userDetailsService;
+    @Autowired
+    public WebSecurityConfig(JWTAuthenticationFilter authenticationFilter, korisnikDetailsService userDetailsService) {
+        this.authenticationFilter = authenticationFilter;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // Create and return a BCryptPasswordEncoder bean
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        var authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+    private static final String[] SECURED_URLs = {"/api/admin/**"};
+
+    private static final String[] UN_SECURED_URLs = {
+            "/api/login/**",
+            "/api/registration/**",
+            "/authenticate/**"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers(UN_SECURED_URLs).permitAll().and()
+                .authorizeHttpRequests().requestMatchers(SECURED_URLs)
+                .hasAuthority("ADMIN").anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
