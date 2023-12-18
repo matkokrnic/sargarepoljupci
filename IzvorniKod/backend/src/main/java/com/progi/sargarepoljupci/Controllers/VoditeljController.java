e29e191aa86913e7f395ec5efe9cbf328b113156
piilip <fm54235@fer.hr>
2023-12-18 03:18:41 +0100

    Basic implementation of Manager endpoints

--- IzvorniKod/backend/src/main/java/com/progi/sargarepoljupci/Controllers/VoditeljController.java
+++ IzvorniKod/backend/src/main/java/com/progi/sargarepoljupci/Controllers/VoditeljController.java
@@ -1,37 +1,41 @@
 package com.progi.sargarepoljupci.Controllers;
 
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.progi.sargarepoljupci.DTO.Request.ParkingInformationRequest;
+import com.progi.sargarepoljupci.Models.ParkingAuto;
+import com.progi.sargarepoljupci.Repository.ParkingAutoRepository;
+import com.progi.sargarepoljupci.Services.ParkingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/voditelj")
 public class VoditeljController {
+    private final ParkingService parkingService;
+    private final ParkingAutoRepository autoRepository;
+    @Autowired
+    public VoditeljController(ParkingService parkingService, ParkingAutoRepository autoRepository) {
+        this.parkingService = parkingService;
+        this.autoRepository = autoRepository;
+    }
 
-    /*
+/*
     // ucrtaj dostupno parkirno mjesto
     @PostMapping
-    public void makeParkingAvailable(){
+    public ResponseEntity<?> makeParkingAvailable(){
+        return null;
+    }
 
+ */
+// Voditelj parkinga ima mogućnost unijeti informacije o svom parkiralištu (naziv, opis,
+// fotografija, cjenik)
+    @PutMapping("/newParking")
+    public ResponseEntity<?> enterParkingInformation(@RequestBody ParkingInformationRequest request){
+        autoRepository.save(new ParkingAuto(request));
+        return ResponseEntity.ok(request);
     }
 
 
-    // Voditelj parkinga ima mogućnost unijeti informacije o svom parkiralištu (naziv, opis,
-    // fotografija, cjenik)
-
-    @PutMapping
-    public void enterParkingInformation(){
-    }
-    // osvjezavanje zauzetosti
-
-
-    @PostMapping
-    public void markParkingSpaces(){
-
-    }
-
-     */
 
 }
