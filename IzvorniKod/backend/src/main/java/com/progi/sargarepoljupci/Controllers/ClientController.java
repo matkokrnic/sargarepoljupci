5a73c5424a11c898070c58c22ae1b5d6c8bb94de
piilip <fm54235@fer.hr>
2023-12-18 03:17:02 +0100

    Very basic implementation of reservation related endpoints

--- IzvorniKod/backend/src/main/java/com/progi/sargarepoljupci/Controllers/ClientController.java
+++ IzvorniKod/backend/src/main/java/com/progi/sargarepoljupci/Controllers/ClientController.java
@@ -1,20 +1,60 @@
 package com.progi.sargarepoljupci.Controllers;
 
 import com.progi.sargarepoljupci.DTO.Request.CalendarReservationRequest;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.progi.sargarepoljupci.DTO.Request.DepositRequest;
+import com.progi.sargarepoljupci.Services.ParkingService;
+import com.progi.sargarepoljupci.Services.WalletService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
 
 
 @RestController
 public class ClientController {
+    private final WalletService walletService;
+    private final ParkingService parkingService;
+    @Autowired
+    public ClientController(WalletService walletService, ParkingService parkingService) {
+        this.walletService = walletService;
+        this.parkingService = parkingService;
+    }
+
+
     // rezervacija putem kalendara
     // parametri: parkiralisna mjesta za koja je klijent zainteresiran, odnosno lista parkiralisnih mjesta
     //
     public void calendarReservation(@RequestBody CalendarReservationRequest request){
 
-    // nadpuna novcanika
+    }
+    // nadopuna novcanika
+    @PutMapping("/{userId}/deposit")
+    public ResponseEntity<String> depositToWallet(@PathVariable Long userId, @RequestBody DepositRequest request) {
+        boolean depositSuccessful = walletService.depositFunds(userId, request.getDepositAmount());
 
+        if (depositSuccessful) {
+            return ResponseEntity.ok("Funds deposited successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed to deposit funds");
+        }
+    }
+
+    @GetMapping("/findNearest")
+    public ResponseEntity<?> findNearestParkingSpot(
+            @RequestParam("destination") String destination,
+            @RequestParam("vehicleType") String vehicleType,
+            //@RequestParam("currentTime") Timestamp currentTime,
+            @RequestParam("parkingDuration") int parkingDurationInMinutes) {
+       return ResponseEntity.ok(parkingService.findNearestAvailableParking(destination, vehicleType, parkingDurationInMinutes));
 
 
     }
+
+
+
+
+
+
 }
--- IzvorniKod/backend/src/main/java/com/progi/sargarepoljupci/Controllers/ReservationController.java
+++ IzvorniKod/backend/src/main/java/com/progi/sargarepoljupci/Controllers/ReservationController.java
@@ -1,12 +1,43 @@
 package com.progi.sargarepoljupci.Controllers;
 
 
-import com.progi.sargarepoljupci.DTO.Request.CalendarReservationRequest;
+import com.progi.sargarepoljupci.DTO.Response.TimeSlotResponse;
+import com.progi.sargarepoljupci.Services.ParkingSpotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ReservationController {
+    private final ParkingSpotService parkingSpotService;
+
+    @Autowired
+    public ReservationController(ParkingSpotService parkingSpotService) {
+        this.parkingSpotService = parkingSpotService;
+    }
+/*
+    @PostMapping("/mapSelection")
+    public ResponseEntity<List<TimeSlotResponse>> handleMapSelection(@RequestBody List<String> ParkingSpotIds){
+        List<TimeSlotResponse> availableTimeSlots = parkingSpotService.checkAvailability(ParkingSpotIds);
+    }
+
+    @PostMapping("/timeSelection")
+    public ResponseEntity<List<ParkingSpot>> handleTimeSlotSelection(@RequestBody TimeSlotRequest timeSlotRequest) {
+
+        List<ParkingSpot> availableParkingSpots = parkingService.getAvailableParkingSpotsForTimeSlot(timeSlotRequest);
+
+        if (!availableParkingSpots.isEmpty()) {
+            return ResponseEntity.ok(availableParkingSpots);
+        } else {
+            return ResponseEntity.noContent().build();
+        }
+    }
+
+ */
 
 
 
