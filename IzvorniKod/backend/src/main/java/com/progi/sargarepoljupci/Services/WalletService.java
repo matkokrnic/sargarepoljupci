0f96b791e94384db684831556df9bb080d56f2f0
piilip <fm54235@fer.hr>
2024-01-18 05:40:02 +0100

    Added the ability to pay with wallet

--- IzvorniKod/backend/src/main/java/com/progi/sargarepoljupci/Services/WalletService.java
+++ IzvorniKod/backend/src/main/java/com/progi/sargarepoljupci/Services/WalletService.java
@@ -1,5 +1,6 @@
 package com.progi.sargarepoljupci.Services;
 
+import com.progi.sargarepoljupci.Exceptions.RequestDeniedException;
 import com.progi.sargarepoljupci.Models.Korisnik;
 import com.progi.sargarepoljupci.Repository.korisnikRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,4 +28,24 @@ public class WalletService {
         }
         return false;
     }
+
+    public void withdrawFunds(Long userId, double amount) {
+        Optional<Korisnik> userOptional = userRepository.findById(userId);
+
+        if (userOptional.isPresent()) {
+            Korisnik user = userOptional.get();
+            double currentBalance = user.getWalletBalance();
+
+            if (currentBalance >= amount) {
+                user.setWalletBalance(currentBalance - amount);
+                userRepository.save(user);
+            } else {
+                throw new RequestDeniedException("Insufficient balance.");
+            }
+        } else {
+            throw new RequestDeniedException("User not found.");
+        }
+    }
+
+
 }
