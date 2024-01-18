fe45f3076dcf70e486a77af0964427e5aef6d054
piilip <fm54235@fer.hr>
2024-01-18 15:31:58 +0100

    PersonalInformation dto for updating user information

---
+++ IzvorniKod/backend/src/main/java/com/progi/sargarepoljupci/DTO/PersonalInformation.java
@@ -0,0 +1,16 @@
+package com.progi.sargarepoljupci.DTO;
+
+import lombok.Data;
+
+@Data
+public class PersonalInformation {
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String photo;
+    private String iban;
+    private String emailAddress;
+
+}
+
