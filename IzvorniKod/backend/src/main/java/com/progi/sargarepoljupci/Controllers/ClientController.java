27f28f2681964bf068b9a01226bd60511f3bde96
piilip <fm54235@fer.hr>
2024-01-18 05:33:08 +0100

    Added Constructor for TimeSlotResponse

--- IzvorniKod/backend/src/main/java/com/progi/sargarepoljupci/Controllers/ClientController.java
+++ IzvorniKod/backend/src/main/java/com/progi/sargarepoljupci/Controllers/ClientController.java
@@ -1,35 +1,42 @@
 package com.progi.sargarepoljupci.Controllers;
 
-import com.progi.sargarepoljupci.DTO.Request.CalendarReservationRequest;
 import com.progi.sargarepoljupci.DTO.Request.DepositRequest;
+import com.progi.sargarepoljupci.DTO.Request.NearestSpotRequest;
+import com.progi.sargarepoljupci.DTO.Response.NearestSpotResponse;
+import com.progi.sargarepoljupci.Exceptions.RequestDeniedException;
+import com.progi.sargarepoljupci.Repository.BicycleRepository;
+import com.progi.sargarepoljupci.Repository.ParkingSpotRepository;
 import com.progi.sargarepoljupci.Services.ParkingService;
+import com.progi.sargarepoljupci.Services.ReservationService;
 import com.progi.sargarepoljupci.Services.WalletService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.Pair;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 
 @RestController
+@RequestMapping("/api/client")
 public class ClientController {
     private final WalletService walletService;
     private final ParkingService parkingService;
+    private final ParkingSpotRepository parkingSpotRepository;
+    private final ReservationService reservationService;
+    private final BicycleRepository bicycleRepository;
     @Autowired
-    public ClientController(WalletService walletService, ParkingService parkingService) {
+    public ClientController(WalletService walletService, ParkingService parkingService, ParkingSpotRepository parkingSpotRepository, ReservationService reservationService, BicycleRepository bicycleRepository) {
         this.walletService = walletService;
         this.parkingService = parkingService;
+        this.parkingSpotRepository = parkingSpotRepository;
+        this.reservationService = reservationService;
+        this.bicycleRepository = bicycleRepository;
     }
 
 
-    // rezervacija putem kalendara
-    // parametri: parkiralisna mjesta za koja je klijent zainteresiran, odnosno lista parkiralisnih mjesta
-    //
-    public void calendarReservation(@RequestBody CalendarReservationRequest request){
 
-    }
     // nadopuna novcanika
     @PutMapping("/{userId}/deposit")
     public ResponseEntity<String> depositToWallet(@PathVariable Long userId, @RequestBody DepositRequest request) {
@@ -42,16 +49,57 @@ public class ClientController {
         }
     }
 
+    // ovo treba vratiti coordinate i boolean je li rezervirano mjesto
     @GetMapping("/findNearest")
-    public ResponseEntity<?> findNearestParkingSpot(
-            @RequestParam("destination") String destination,
-            @RequestParam("vehicleType") String vehicleType,
-            //@RequestParam("currentTime") LocalDateTime currentTime,
-            @RequestParam("parkingDuration") int parkingDurationInMinutes) {
-       return ResponseEntity.ok(parkingService.findNearestAvailableParking(destination, vehicleType, parkingDurationInMinutes));
+    public ResponseEntity<NearestSpotResponse> findNearestParkingSpot(
+            @RequestBody NearestSpotRequest nearestSpotRequest) {
+        var vehicleType = nearestSpotRequest.getVehicleType();
+        //var currentTime = nearestSpotRequest.getCurrentTime();
+        var parkingDurationInMinutes = nearestSpotRequest.getParkingDurationInMinutes();
+        var destination = nearestSpotRequest.getLatitude()+","+nearestSpotRequest.getLongitude();
+        var currentTime = LocalDateTime.now();
 
+
+
+        // ako su bicikli onda samo vratimo koordinate najblizeg, ne trebamo radit nista sto se tice rezervacije
+        if(vehicleType.equalsIgnoreCase("bicycle")){
+            var nearest= parkingService.findNearestBicycleSpot(destination);
+            var bicycleParking = bicycleRepository.findByLongitudeAndLatitude(nearest.getFirst(), nearest.getSecond());
+            if(bicycleParking==null)
+                throw new RequestDeniedException("There's no available bicycle parking");
+            return ResponseEntity.ok(new NearestSpotResponse(nearest.getFirst(), nearest.getSecond(), false, bicycleParking.getBicycle_id()));
+        }
+        currentTime = reservationService.roundToClosest30Minutes(currentTime);
+       Pair<Double, Double> nearestCoordinates = parkingService.findNearestAvailableParking(destination, currentTime);
+        // provjeriti je li rezervirano mjesto, prvo nadjemo mjesto
+       var parkingSpot = parkingSpotRepository.findByLongitudeAndLatitude(nearestCoordinates.getFirst(), nearestCoordinates.getSecond());
+        LocalDateTime reservationEnd = currentTime.plusMinutes(parkingDurationInMinutes);
+
+
+
+        var reservable = reservationService.canParkingSpotBeReserved(parkingSpot.getId(), currentTime,  reservationEnd) && parkingSpot.getReservable();
+        //if(reservable){
+        //    reservationService.makeMultipleReservations(List.of(new TimeSlotRequest(currentTime, currentTime.plusMinutes(parkingDurationInMinutes))), )
+        //}
+        return ResponseEntity.ok(new NearestSpotResponse(nearestCoordinates.getFirst(), nearestCoordinates.getSecond(), reservable, parkingSpot.getId()));
 
     }
+
+    @PostMapping("/{userId}/subtractBalance")
+    public ResponseEntity<String> withdrawFunds(@PathVariable Long userId, @RequestParam double amount) {
+        try {
+            walletService.withdrawFunds(userId, amount);
+            return ResponseEntity.ok("Balance subtracted successfully.");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+
+
+
+
+
 
 
 
--- IzvorniKod/backend/src/main/java/com/progi/sargarepoljupci/DTO/Response/TimeSlotResponse.java
+++ IzvorniKod/backend/src/main/java/com/progi/sargarepoljupci/DTO/Response/TimeSlotResponse.java
@@ -1,5 +1,6 @@
 package com.progi.sargarepoljupci.DTO.Response;
 
+import com.progi.sargarepoljupci.Models.Reservation;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -8,5 +9,19 @@ public class TimeSlotResponse {
     private LocalDateTime startTime;
     private LocalDateTime endTime;
     private String parkingSpotId;
+
+    public TimeSlotResponse(LocalDateTime startTime, LocalDateTime endTime, String parkingSpotId) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.parkingSpotId = parkingSpotId;
+    }
+
+    public TimeSlotResponse() {
+    }
+    public TimeSlotResponse(Reservation reservation) {
+        this.startTime = reservation.getReservationStart();
+        this.endTime = reservation.getReservationEnd();
+        this.parkingSpotId = reservation.getParkingSpot().getId();
+    }
 
 }
