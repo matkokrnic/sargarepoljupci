ec37f393cb87d0fb9586dfc762faabd351d90ec7
domajenjic <149622668+domajenjic@users.noreply.github.com>
2024-01-19 20:09:03 +0100

    Azuriran port

--- IzvorniKod/backend/src/main/java/com/progi/sargarepoljupci/Security/CorsConfig.java
+++ IzvorniKod/backend/src/main/java/com/progi/sargarepoljupci/Security/CorsConfig.java
@@ -25,7 +25,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("http://localhost:5173");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
