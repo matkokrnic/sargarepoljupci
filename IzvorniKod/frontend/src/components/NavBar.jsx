6b2025ca0c85cb4b63f3d69a8761f64538c30aab
MegaNoris <lv53154@fer.hr>
2024-01-20 00:50:51 +0100

    Modificirao NavBar

--- IzvorniKod/frontend/src/components/NavBar.jsx
+++ IzvorniKod/frontend/src/components/NavBar.jsx
@@ -3,23 +3,94 @@ import { Fragment} from 'react'
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import { useLocation } from 'react-router-dom';
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import { UserCircleIcon} from '@heroicons/react/24/solid'
+import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { UserCircleIcon} from '@heroicons/react/24/solid';
 import {logout2} from '../features/auth/authService';
+import { useSelector } from 'react-redux';
 
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
+
+function Profile() {
+  return(<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+  <button
+    type="button"
+    className="relative p-1 text-gray-400 bg-gray-800 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+  >
+    <span className="absolute -inset-1.5" />
+    <span className="sr-only">Pogledajte obavijesti</span>
+    <BellIcon className="w-6 h-6" aria-hidden="true" />
+  </button>
+
+  {/* Profile dropdown */}
+  <Menu as="div" className="relative ml-3">
+    <div>
+      <Menu.Button className="relative flex text-sm bg-gray-800 rounded-full focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+        <span className="absolute -inset-1.5" />
+        <span className="sr-only">Otvori korisnički izbornik</span>
+        <UserCircleIcon className="w-8 h-8 text-gray-300" aria-hidden="true" />
+      </Menu.Button>
+    </div>
+    <Transition
+      as={Fragment}
+      enter="transition ease-out duration-100"
+      enterFrom="transform opacity-0 scale-95"
+      enterTo="transform opacity-100 scale-100"
+      leave="transition ease-in duration-75"
+      leaveFrom="transform opacity-100 scale-100"
+      leaveTo="transform opacity-0 scale-95"
+    >
+      <Menu.Items className="absolute right-0 z-10 w-48 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Item>
+          {({ active }) => (
+            <a
+              href="/profile"
+              className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+            >
+              Vaš profil
+            </a>
+          )}
+        </Menu.Item>
+        <Menu.Item>
+          {({ active }) => (
+            <a
+              href="/settings"
+              className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+            >
+              Postavke
+            </a>
+          )}
+        </Menu.Item>
+        <Menu.Item>
+          {({ active }) => (
+            <a
+              href="/"
+              onClick={logout2}
+              className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+            >
+              Odjavi se
+            </a>
+          )}
+        </Menu.Item>
+      </Menu.Items>
+    </Transition>
+  </Menu>
+</div>)
+}
+
 export function NavBar() {
   const navigation = [
-    { name: 'Nadzorna ploča', href: '/home', current: false },
-    { name: 'Karta', href: '/map', current: false },
-    { name: 'Statistika', href: '/stats', current: false },
-    { name: 'Novčanik', href: '/wallet', current: false },
+    { name: 'Nadzorna ploča', href: '/home', current: false, log : false },
+    { name: 'Karta', href: '/map', current: false, log : false },
+    { name: 'Statistika', href: '/stats', current: false, log : true },
+    { name: 'Novčanik', href: '/wallet', current: false, log : true }
   ];
   const {hash, pathname, search} = useLocation();
+  const auth = useSelector(state => state.auth);
+  const Prof = (auth.user)? Profile() : <></>;
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
@@ -50,8 +121,9 @@ export function NavBar() {
                 </a>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <a
+                    {navigation.map((item) => {
+                      if (!(item.log && !auth.user))
+                      return(<a
                         key={item.name}
                         href={item.href}
                         className={classNames(
@@ -59,85 +131,23 @@ export function NavBar() {
                           'rounded-md px-3 py-2 text-sm font-medium'
                         )}
                         //style = {(pathname == item.href)? {backgroundcolor: "blue"} : {}}
-                        aria-current={item.current ? 'page' : undefined}
+                        aria-current={(pathname == item.href) ? 'page' : undefined}
                       >
                         {item.name}
-                      </a>
-                    ))}
+                      </a>)
+                    })}
                   </div>
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="relative p-1 text-gray-400 bg-gray-800 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">Pogledajte obavijesti</span>
-                  <BellIcon className="w-6 h-6" aria-hidden="true" />
-                </button>
-
-                {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-3">
-                  <div>
-                    <Menu.Button className="relative flex text-sm bg-gray-800 rounded-full focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="absolute -inset-1.5" />
-                      <span className="sr-only">Otvori korisnički izbornik</span>
-                      <UserCircleIcon className="w-8 h-8 text-gray-300" aria-hidden="true" />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 w-48 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="/profile"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Vaš profil
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="/settings"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Postavke
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="/"
-                            onClick={logout2}
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Odjavi se
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-              </div>
+              {Prof}
             </div>
           </div>
 
           <Disclosure.Panel className="sm:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
-              {navigation.map((item) => (
-                <Disclosure.Button
+              {navigation.map((item) => {
+                if (!(item.log && !auth.user))
+                return (<Disclosure.Button
                   key={item.name}
                   as="a"
                   href={item.href}
@@ -146,11 +156,11 @@ export function NavBar() {
                     'block rounded-md px-3 py-2 text-base font-medium'
                   )}
                   //style = {(pathname == item.href)? {backgroundcolor: "blue"} : {}}
-                  aria-current={item.current ? 'page' : undefined}
+                  aria-current={(pathname == item.href) ? 'page' : undefined}
                 >
                   {item.name}
-                </Disclosure.Button>
-              ))}
+                </Disclosure.Button>)
+                })}
             </div>
           </Disclosure.Panel>
         </>
--- IzvorniKod/frontend/src/features/store.js
+++ IzvorniKod/frontend/src/features/store.js
@@ -3,6 +3,6 @@ import authReducer from '../features/auth/authSlice'
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer,
-    },
+        auth: authReducer
+    }
 })
